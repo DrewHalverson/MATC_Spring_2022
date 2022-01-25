4b4c28db3c4de17e5986bf0c1a8a82f2adb5f074
Drew Halverson <drew@navlys.com>
2022-01-25 09:44:02 -0600

    refactored lab 1

--- advancedJS/Labs/Lab1/js/lab1.js
+++ advancedJS/Labs/Lab1/js/lab1.js
@@ -1,13 +1,28 @@
-function validate() {
+// initial submission
 
-    // Declare and assign variables
-    let form = document.getElementById("form");
-    let output = form.username.value;
+// function validate() {
 
-    // validate if user has entered a username in text field and output appropriate message
-    if (output == "") {
+//     // Declare and assign variables
+//     let form = document.getElementById("form");
+//     let output = form.username.value;
+
+//     // validate if user has entered a username in text field and output appropriate message
+//     if (output == "") {
+//         alert("username is a required field");
+//     } else {
+//         alert("You have entered " + output + " as the user name.");
+//     }
+// }
+
+
+// refactored
+
+const validate = () => {
+    let usernameField = document.querySelector("#username");
+    console.log(usernameField.value);
+    if (username.value == "") {
         alert("username is a required field");
     } else {
-        alert("You have entered " + output + " as the user name.");
+        alert("You have entered " + usernameField.value + " as the user name.");
     }
 }
