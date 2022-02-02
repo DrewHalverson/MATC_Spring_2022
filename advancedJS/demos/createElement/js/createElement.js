3c82e82775efc21dd55a0c4f7fa421fca8adff64
Drew Halverson <drew@navlys.com>
2022-02-02 17:16:03 -0600

    updates to course assignments

---
+++ advancedJS/demos/createElement/js/createElement.js
@@ -0,0 +1,43 @@
+// let element = document.createElement(htmlTag);
+
+const createElementDemo = () => {
+    // let div = document.createElement('div');
+
+    // div.id = 'content';
+
+    // div.className = 'note';
+
+
+
+    // div.innerHTML = '<p>CreateElement example</p>';
+
+    // let text = document.createTextNode("CreateElement Example txtnode" );
+    // div.appendChild(text);
+
+    // let h2 = document.createElement('h2');
+    // h2.textContent = "Add h2 element to the div";
+    // div.appendChild(h2);
+
+    // document.body.appendChild(div)
+
+    // let li = document.createElement('li');
+    // li.textContent = 'Products';
+
+    // li = document.createElement('li');
+    // li.textContent = 'About us';
+
+    // const menu = document.querySelector('#menu');
+    // menu.appendChild(li);
+
+    // select the ul menu element
+const menu = document.querySelector('#menu');
+menu.appendChild(li);
+}
+
+let li = document.createElement('li');
+li.textContent = 'Products';
+menu.appendChild(li);
+
+li = document.createElement('li');
+li.textContent = 'About Us';
+
