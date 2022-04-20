54b53c38e6b780905832d0ab7c39e4d17bed180d
Drew Halverson <drew@navlys.com>
2022-04-20 15:19:36 -0500

    Final draft of project 3. Some UI/UX updates

--- advancedJS/projects/project3/js/project3.js
+++ advancedJS/projects/project3/js/project3.js
@@ -22,6 +22,7 @@ const init = () => {
         xhr.onreadystatechange = () => {
             if(xhr.readyState ==4) {
                 console.log("new record was added");
+                location.reload();
             }
         }
 
@@ -49,32 +50,38 @@ const init = () => {
 
                 let tasks = taskData.Items;
 
-                console.log(tasks);
-
                 for(let i = 0; i < tasks.length; i++) {
+
+                    let task = `${tasks[i].Description}`;
 
                     // create delete button
                     let deleteButton = document.createElement("button");
+                    let deleteID = task + "Button";
                     deleteButton.setAttribute("type", "button");
                     deleteButton.setAttribute("name", "deleteButton");
-                    deleteButton.setAttribute("id", "deleteButton");
+                    deleteButton.setAttribute("id", '"' + deleteID + '"' );
                     deleteButton.setAttribute("value", "Delete");
                     deleteButton.setAttribute("class", "btn d-inline-block");
                     deleteButton.innerHTML = '<img src="images/delete-icon.png" width="25" height="25"/>';
+                    
+                    // event listener for delete button
+                    deleteButton.addEventListener("click", () => {
+                        deleteTask(task)
+                    });
 
                     // create paragraph for task description
-                    let task = `${tasks[i].Description}`;
                     let taskText = document.createElement("p");
-                    taskText.setAttribute("id", "taskText");
+                    taskText.setAttribute("id", '"' + task + '"');
                     taskText.setAttribute("class", "d-inline-block mx-4");
                     taskText.innerHTML = task;
-                    
+
                     // create div for task item
                     let taskDiv = document.createElement("div");
                     taskDiv.setAttribute("id", "taskDiv");
                     taskDiv.appendChild(deleteButton);
                     taskDiv.appendChild(taskText);
 
+                    // output task to web page
                     document.getElementById("main").appendChild(taskDiv);
                 }               
             }
@@ -82,24 +89,17 @@ const init = () => {
         xhr.send(null);
     }
 
-    // runs the getTasks function to retrieve the task list on page load. 
-    onload = getTasks();
+    // deletes a task 
+    const deleteTask = (task) => {
 
-    // deletes a task  - doesn't work unless description is hard coded. 
-    //I wasn't able to find an example of how to do this in any of the module
-    //material. 
-    
-    const deleteTask = () => {
-    
             let xhr = new XMLHttpRequest();
             let url = "https://ghu8xhzgfe.execute-api.us-east-1.amazonaws.com/tasks";
             let apiKey = "Itcheui2tB58SlUGe8rrP8mskudGsNDT9nfKKG9S";
-            let studentId = "2694082"
-            //let taskDescription = document.querySelector("#taskText").value;
+            let studentId = "2694082";
 
             let params = {
                 "StudentId" : studentId,
-                "Description" : "test1"  
+                "Description" : task
             };
     
             xhr.open("delete", url);
@@ -110,14 +110,18 @@ const init = () => {
             xhr.onreadystatechange = () => {
                 if(xhr.readyState ==4) {
                     console.log("task was deleted");
+                    location.reload();
                 }
             }
     
             xhr.send(JSON.stringify(params));
     }
 
+    // runs the getTasks function to retrieve the task list on page load. 
+    onload = getTasks();
+
+    // event listener to add a new task on button click
     document.querySelector("#addNewTask").addEventListener("click", addNewTask);
-    document.querySelector("main").addEventListener("click", deleteTask);   
 }
 
 window.onload = init;
